696ae5bb0005c3d95e879b0bfcaa0968d27589dd
Gonzalo Romero <gondragon13@gmail.com>
2017-09-25 13:49:05 -0300

    Added Carpet Tool Mod

---
+++ CarpetTool/items/carpetTool/carpetcolor.lua
@@ -0,0 +1,39 @@
+CarpetColor = WeaponAbility:new()
+
+function CarpetColor:init()
+  self.ammoIndex = math.min(config.getParameter("ammoIndex", 1), #self.ammoTypes)
+  self:adaptAbility()
+
+  self.weapon.onLeaveAbility = function()
+    self.weapon:setStance(self.stances.idle)
+  end
+end
+
+function CarpetColor:update(dt, fireMode, shiftHeld)
+  WeaponAbility.update(self, dt, fireMode, shiftHeld)
+
+  if not self.weapon.currentAbility and self.fireMode == (self.activatingFireMode or self.abilitySlot) then
+    self:setState(self.switch)
+  end
+end
+
+function CarpetColor:switch()
+  self.ammoIndex = (self.ammoIndex % #self.ammoTypes) + 1
+  activeItem.setInstanceValue("ammoIndex", self.ammoIndex)
+
+  self:adaptAbility()
+  animator.playSound("switchAmmo")
+
+  self.weapon:setStance(self.stances.switch)
+
+  util.wait(self.stances.switch.duration)
+end
+
+function CarpetColor:adaptAbility()
+  local ability = self.weapon.abilities[self.adaptedAbilityIndex]
+  util.mergeTable(ability, self.ammoTypes[self.ammoIndex])
+  animator.setAnimationState("ammoType", tostring(self.ammoIndex))
+end
+
+function CarpetColor:uninit()
+end
